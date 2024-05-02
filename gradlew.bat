1de987d728a1af0d0b8951f1f19e1597fb093061
maropboia <164220066+maropboia@users.noreply.github.com>
2024-05-02 12:46:17 +0600

    comment

--- gradlew.bat
+++ gradlew.bat
@@ -1,31 +1,19 @@
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
-
-@if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
-@rem
+@rem ****************************************************************************
+@rem ****************************************************************************
+@rem 
 @rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+@rem 
+@rem ****************************************************************************
+@rem ****************************************************************************
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
+@rem Set the directory name of the current script
+if "%~dp0" == "" set DIRNAME=.
 set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
+
+@rem Set the base name of the current script
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
@@ -51,7 +39,7 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_HOME=%JAVA_EXE:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
@@ -69,11 +57,9 @@ goto fail
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
-:end
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
